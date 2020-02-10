04b2f3a9064dd7cf9f18b8b6da10500d4f6175a7
Nicholas Juniper <njuniper99@gmail.com>
2020-02-10 14:56:22 -0500

    Pushed Raymarching

---
+++ RAYMARCH1/Assets/CameraMarchingMaster.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class CameraMarchingMaster : MonoBehaviour {
+
+    // First we need a compute shader 
+    public ComputeShader raymarching;
+
+    // this is where we will be storing the results of our ray marching
+    RenderTexture target;
+
+    // this is our unity camera 
+    Camera cam;
+
+    // we need to clean up each frame 
+    List<ComputeBuffer> buffersToDispose;
+
+    // we will need a light position
+    Light lightSource;
+
+    //////////////
+    void Init () {
+        cam = Camera.current;
+        buffersToDispose = new List<ComputeBuffer> ();
+        lightSource = FindObjectOfType<Light> ();
+
+    }
+
+    // this is called when we need to re-render
+    void OnRenderImage (RenderTexture source, RenderTexture destination) {
+        Init ();
+        InitRenderTexture ();
+        CreateScene ();
+        SetParameters ();
+
+        // setup our pointers to variables in the compute shader
+        raymarching.SetTexture (0, "Source", source);
+        raymarching.SetTexture (0, "Destination", target);
+
+        // tell the compute shader to start
+        int threadGroupsX = Mathf.CeilToInt (cam.pixelWidth / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt (cam.pixelHeight / 8.0f);
+        raymarching.Dispatch (0, threadGroupsX, threadGroupsY, 1);
+
+        // draw the resulting texture to the screen
+        Graphics.Blit (target, destination);
+
+        // cleanup
+        foreach (var buffer in buffersToDispose) {
+            buffer.Dispose ();
+        }
+    }
+
+    // Create the scene
+    // this just packs the data in the unity scene graph into a contiguous array 
+    // so that we can send it to the compute shader
+    void CreateScene () {
+        // get all shapes in the scene graph
+        List<Shape> allShapes = new List<Shape> (FindObjectsOfType<Shape> ());
+
+        // create an array of shape data (guaranteed to be a contiguous buffer)
+        ShapeData[] shapeData = new ShapeData[allShapes.Count];
+
+        // go through and copy the data into the buffer
+        for (int i = 0; i < allShapes.Count; i++) {
+            var s = allShapes[i];
+            Vector3 col = new Vector3 (s.colour.r, s.colour.g, s.colour.b);
+            shapeData[i] = new ShapeData () {
+	                position 	= s.Position,
+	                scale 		= s.Scale, 
+	                colour 		= col,
+	                shapeType 	= (int) s.shapeType,
+	                operation 	= (int) s.operation,
+	                blendStrength = s.blendStrength*3,
+            };
+        }
+
+        // create a compute buffer (StructuredBuffer) to send to the compute shader
+        ComputeBuffer shapeBuffer = new ComputeBuffer (shapeData.Length, ShapeData.GetSize ());
+
+        // this tells the compute shader where to find the data
+        shapeBuffer.SetData (shapeData);
+        raymarching.SetBuffer (0, "shapes", shapeBuffer);
+        raymarching.SetInt ("numShapes", shapeData.Length);
+
+        // for cleanup
+        buffersToDispose.Add (shapeBuffer);
+    }
+
+    // this just sets some global variables in the computeShader
+    void SetParameters () {
+        raymarching.SetMatrix ("_CameraToWorld", cam.cameraToWorldMatrix);
+        raymarching.SetMatrix ("_CameraInverseProjection", cam.projectionMatrix.inverse);
+        raymarching.SetVector ("_LightPosition", lightSource.transform.position);
+
+    }
+
+    // create a texture target to render to
+    void InitRenderTexture () {
+        if (target == null || target.width != cam.pixelWidth || target.height != cam.pixelHeight) {
+            if (target != null) {
+                target.Release ();
+            }
+            target = new RenderTexture (cam.pixelWidth, cam.pixelHeight, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            target.enableRandomWrite = true;
+            target.Create ();
+        }
+    }
+
+    // definition of the actual shape data, this needs to be in the exact same ordering as in the computeShader
+    struct ShapeData {
+        public Vector3 position;
+        public Vector3 scale;
+        public Vector3 colour;
+        public int shapeType;
+        public int operation;
+        public float blendStrength;
+
+        // this function returns the number of bytes, in our case above we have 10 floats and 2 ints
+        public static int GetSize () {
+            return sizeof (float) * 10 + sizeof (int) * 2;
+        }
+    }
+}
---
+++ RAYMARCH1/Assets/Shape.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this is just a simple shape that contains a bit of information on blending operation and type for our compute shader
+public class Shape : MonoBehaviour
+{
+    public enum ShapeType {Sphere,Cube,Torus};
+    public enum Operation {None, Blend, Cut,Mask}
+
+    public ShapeType shapeType;
+    public Operation operation;
+    public Color colour = Color.white;
+
+    [Range(0,1)]
+    public float blendStrength;
+    public bool isAnimated = false;
+
+    [Range(0,0.5f)]
+    public float animX=0.0f;
+    [Range(0,0.5f)]
+    public float animY=0.01f;
+    [Range(0,0.5f)]
+    public float animZ=0;
+
+    public Vector3 Position {
+        get {
+            return transform.position;
+        }
+    }
+
+    public Vector3 Scale {
+        get {
+            return transform.localScale;
+        }
+    }
+    void Update(){
+        if(isAnimated){
+            transform.position += new Vector3(animX*Mathf.Cos(Time.time),0.01f*Mathf.Sin(Time.time),animZ*Mathf.Sin(Time.time*2.0f));
+        }   
+    }
+}
